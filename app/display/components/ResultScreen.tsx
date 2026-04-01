de6024074d348ea7e7c8f021d814c4e2edb36bbe
谷口愛利 <taniguchi.airi1170@alphatc.co.jp>
2026-04-01 16:28:21 +0900

    正解者の名前表示を追加

--- app/display/components/ResultScreen.tsx
+++ app/display/components/ResultScreen.tsx
@@ -1,4 +1,42 @@
+"use client"
+
+import { useEffect, useRef } from "react";
+
+const groomWinners = ["山田太郎", "鈴木一郎", "田中次郎","山田太郎", "鈴木一郎", "田中次郎","山田太郎", "鈴木一郎", "田中次郎","鈴木一郎", "田中次郎","山田太郎", "鈴木一郎","山田太郎", "鈴木一郎", "田中次郎","山田太郎", "鈴木一郎", "田中次郎","山田太郎", "鈴木一郎",];
+const brideWinners = ["佐藤花子", "高橋美咲", "伊藤愛"];
+
 export default function ResultScreen() {
+  const groomRef = useRef<HTMLDivElement | null>(null);
+  const brideRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (groomRef.current) {
+        const el = groomRef.current;
+
+        // 一番下に到達したらリセット
+        if (el.scrollTop + el.clientHeight >= el.scrollHeight) {
+          el.scrollTop = 0;
+        } else {
+          el.scrollTop += 1;
+        }
+      }
+
+      if (brideRef.current) {
+        const el = brideRef.current;
+
+        if (el.scrollTop + el.clientHeight >= el.scrollHeight) {
+          el.scrollTop = 0;
+        } else {
+          el.scrollTop += 1;
+        }
+      }
+    }, 30);
+
+    return () => clearInterval(interval);
+  }, []);
+
+
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>🎉 正解発表 🎉</h1>
@@ -9,8 +47,37 @@ export default function ResultScreen() {
       </div>
 
       <p style={styles.subText}>正解者：80名</p>
+
+      {/* 新郎・新婦エリア */}
+      <div style={styles.wrapper}>
+      {/* 新郎側 */}
+      <div>
+        <h3>新郎側</h3>
+
+        <div ref={groomRef} style={styles.scrollBox}>
+          {groomWinners.map((name, index) => (
+            <p key={index} style={styles.name}>
+              {name}
+            </p>
+          ))}
+        </div>
+      </div>
+
+      {/* 新婦側 */}
+      <div>
+        <h3>新婦側</h3>
+
+        <div ref={brideRef} style={styles.scrollBox}>
+          {brideWinners.map((name, index) => (
+            <p key={index} style={styles.name}>
+              {name}
+            </p>
+          ))}
+        </div>
+  </div>
+</div>
     </div>
-  )
+  );
 }
 
 const styles = {
@@ -19,7 +86,7 @@ const styles = {
     marginTop: "100px",
   },
   title: {
-    fontSize: "40px",
+    fontSize: "clamp(24px, 4vw, 48px)",
     marginBottom: "40px",
   },
   answerBox: {
@@ -37,4 +104,27 @@ const styles = {
     fontSize: "20px",
     color: "#666",
   },
-}
+
+  // 追加
+  wrapper: {
+    display: "flex",
+    justifyContent: "center",
+    gap: "40px",
+    marginTop: "40px",
+  },
+  scrollBox: {
+    width: "25vw",
+    height: "60vh",
+    maxWidth: "400px",
+    minWidth: "250px",
+    maxHeight: "600px",
+    minHeight: "300px",
+    overflow: "hidden",
+    border: "1px solid #ccc",
+  },
+
+  name: {
+    padding: "5px",
+    borderBottom: "1px solid #ccc",
+  },
+};
