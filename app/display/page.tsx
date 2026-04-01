1861e7a52e03ed0ce7b73254f0a3be931fd90760
A T <c5aaa.p3v@icloud.com>
2026-04-02 08:02:39 +0900

    ディスプレイ画面をFirebaseと連携

--- app/display/page.tsx
+++ app/display/page.tsx
@@ -1,10 +1,30 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { doc, onSnapshot } from "firebase/firestore"
+import { db } from "@/app/lib/firebase"
+
 import ClosedScreen from "./components/CloseScreen"
 import ResultScreen from "./components/ResultScreen"
 import LotteryScreen from "./components/LotteryScreen"
 import WinnerScreen from "./components/WinnerScreen"
 
 export default function DisplayPage() {
-  const status = "result" // ← ここ変えると画面切り替わる
+  const [status, setStatus] = useState("waiting")
+
+  useEffect(() => {
+    const unsub = onSnapshot(
+      doc(db, "quizzes", "test-quiz", "state", "current"),
+      (doc) => {
+        const data = doc.data()
+        if (data) {
+          setStatus(data.phase)
+        }
+      }
+    )
+
+    return () => unsub()
+  }, [])
 
   if (status === "closed") {
     return <ClosedScreen />
